c604c1f50e8f882fbc90b84506b8e3e175611ace
ChoroCoDev <choroco.dev@gmail.com>
2023-08-08 23:11:32 +0900

    #2. Header作成

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,6 +10,7 @@ const Header = () => {
     <div className='container flex justify-between'>
       <h1 className='text-4xl font-bold hover:text-red-700/80 duration-300'>PartyManageApp</h1>
       <ul className='flex flex-row '>
+        {/* サインインしていない場合は表示しないようにしてもいいかも？ */}
         {items.map((item, index) => (
           <li className='mx-2 my-1 hover:text-blue-400 hover:underline' key={'header-item-' + index}>
             {item}
