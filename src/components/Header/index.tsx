857b36c529c551a4ea8fe79812a2f3ebb4f3de50
ChoroCoDev <choroco.dev@gmail.com>
2023-08-08 23:10:04 +0900

    #2. Header作成

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+import UserMenu from '@/components/Login/UserMenu'
+import React, { useMemo } from 'react'
+
+const Header = () => {
+  const items = useMemo(() => {
+    return [<p>MyPage</p>, <p>PartyPage</p>, <UserMenu />]
+  }, [])
+  return (
+    <div className='container flex justify-between'>
+      <h1 className='text-4xl font-bold hover:text-red-700/80 duration-300'>PartyManageApp</h1>
+      <ul className='flex flex-row '>
+        {items.map((item, index) => (
+          <li className='mx-2 my-1 hover:text-blue-400 hover:underline' key={'header-item-' + index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Header
